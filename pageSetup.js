e78a55c1ed7e579c17817c4465bf8fb051178e75
thomasgengel34 <thomasgengel34@yahoo.com>
2018-10-09 17:30:02 -0500

    10/9/2018 changes

---
+++ pageSetup.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+//$(document).ready(function () { 
+//    displayHomePage();
+//});
+
+$('#home').click(displayHomePage);
+
+$('#about').click(displayAboutPage);
+
+function displayHomePage() {
+    $('#displayHomePage').show();
+    $('#displayAboutPage').hide(); 
+}
+
+function displayAboutPage() {
+    $('#displayHomePage').hide();
+    $('#displayAboutPage').show();
+}
